881a61c66602dc03845fde70493eb7561e2fb98f
YeJinHong <teepij@naver.com>
2023-07-10 21:56:51 +0900

    solve 인사고과

--- PRG/인사고과.java
+++ PRG/인사고과.java
@@ -1,5 +1,8 @@
 package PRG;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/152995?language=java
 
 class Solution {
@@ -36,6 +39,7 @@ class Solution {
 			pq.add(score);
 		}
 		
+		Score old = new Score(-1, -1, -1);
 		LinkedList<Score> highList = new LinkedList<>();
 		highList.add(new Score(-1, -1, -1));
 		int rank = 0;
@@ -45,15 +49,18 @@ class Solution {
 			Score score = pq.poll();
 //			System.out.println(score);
 			// 인센티브를 받을 자격이 없는 경우
+			// score보다 점수의 합이 높은 리스트에서 검사한다.
+			// 어떤 사원이 다른 임의의 사원보다 두 점수가 모두 낮은 경우 => 임의의 사원은 두 점수의 합이 높은 사원 중에 있다.
 			if(check(highList, score)) {
+				highList.add(score);
 				continue;
 			}
 			
-			Score last = highList.getLast();
-			// 인센티브를 받을 수 있으나 동급이 있는 경우
-			if((last.A + last.B) == (score.A + score.B)) {
+			
+			// 내 앞 순서와 비교한다. 인센티브를 받을 수 있으나 동급이 있는 경우
+			if((old.A + old.B) == (score.A + score.B)) {
 				sameRankCount++;
-			} else {
+			} else { 
 				rank += sameRankCount;
 				sameRankCount = 1;
 			}
@@ -62,6 +69,10 @@ class Solution {
 				answer = rank;
 				break;
 			}
+			
+			highList.add(score);
+			old = score;
+			
 		}
 
 		return answer;
